62cbf84c691c803679c3c30016f1be91b40be785
Ahmed_Ehab <ahmed_ehab707@yahoo.com>
2020-02-22 14:34:11 +0200

    - Transforming gradle files to KTS files.

---
+++ app/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins{
+    id("com.android.application")
+    kotlin("android") //= "kotlin-android"
+    kotlin("android.extensions") //= "kotlin-android-extensions"
+}
+
+android {
+    compileSdkVersion(29)
+    buildToolsVersion = "29.0.2"
+
+    defaultConfig {
+        applicationId = "com.vodafone.kotlindslexample"
+        minSdkVersion(17)
+        targetSdkVersion(29)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("debug") {
+            isTestCoverageEnabled = true
+        }
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+
+    androidExtensions {
+        isExperimental = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation ("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.61")
+    implementation ("androidx.appcompat:appcompat:1.1.0")
+    implementation ("androidx.core:core-ktx:1.2.0")
+    implementation ("androidx.constraintlayout:constraintlayout:1.1.3")
+    testImplementation ("junit:junit:4.12")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.1")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.2.0")
+}
